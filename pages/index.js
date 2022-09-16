ba41d5d615df464a9773f29754471414edbe97fc
Masson LeMieux <fml45678@gmail.com>
2022-09-16 08:44:24 -0400

    organized json files into data folder, added new json files and images, added margin bottom in layout module for index.js container

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import ItemThumbnail from "../components/ItemThumbnail";
 import styles from "../styles/Home.module.css";
-import data from "../public/Craft-Sewing-Patterns.json";
-import toyData from "../public/TOY.json";
+import patternData from "../public/data/Craft-Sewing-Patterns.json";
+import toyData from "../public/data/TOY.json";
+import speakersData from "../public/data/Electric-Audio-Speakers.json";
+import multiData from "../public/data/Electric-Device-Multi.json";
+import phonesData from "../public/data/Electric-Device-Phones.json";
+import screensData from "../public/data/Electric-Device-Screens.json";
+import keyboardsData from "../public/data/Electronics-Computer-Keyboards.json";
 
 export default function Home() {
   return (
@@ -16,14 +21,44 @@ export default function Home() {
         </Head>
         <h1 className={styles.section}>CRAFT</h1>
         <div className={styles.flexContainer}>
-          {data.map((data, key) => (
+          {patternData.map((data, key) => (
             <ItemThumbnail cat="CSP" key={key} img={data.SKU} />
           ))}
         </div>
-        <h1 className={styles.section}>CRAFT</h1>
+        <h1 className={styles.section}>TOY</h1>
         <div className={styles.flexContainer}>
-          {toyData.map((data, key) => (
-            <ItemThumbnail cat="TOY" key={key} img={data.SKU} />
+          {toyData.map((data) => (
+            <ItemThumbnail cat="TOY" key={data.SKU} img={data.SKU} />
+          ))}
+        </div>
+        <h1 className={styles.section}>Speakers</h1>
+        <div className={styles.flexContainer}>
+          {speakersData.map((data) => (
+            <ItemThumbnail cat="EAS" key={data.SKU} img={data.SKU} />
+          ))}
+        </div>
+        <h1 className={styles.section}>Multi</h1>
+        <div className={styles.flexContainer}>
+          {multiData.map((data) => (
+            <ItemThumbnail cat="EDM" key={data.SKU} img={data.SKU} />
+          ))}
+        </div>
+        <h1 className={styles.section}>Phones</h1>
+        <div className={styles.flexContainer}>
+          {phonesData.map((data) => (
+            <ItemThumbnail cat="EDP" key={data.SKU} img={data.SKU} />
+          ))}
+        </div>
+        <h1 className={styles.section}>Screens</h1>
+        <div className={styles.flexContainer}>
+          {screensData.map((data) => (
+            <ItemThumbnail cat="EDS" key={data.SKU} img={data.SKU} />
+          ))}
+        </div>
+        <h1 className={styles.section}>Keyboards</h1>
+        <div className={styles.flexContainer}>
+          {keyboardsData.map((data) => (
+            <ItemThumbnail cat="ECK" key={data.SKU} img={data.SKU} />
           ))}
         </div>
       </div>
